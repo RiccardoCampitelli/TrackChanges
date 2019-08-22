f7523f26529d6c9c0e0c696e134885aa06a2720c
RiccardoCampitelli <riccardo.campitelli91@gmail.com>
2019-08-22 10:12:55 +0100

    added readme

--- Program.cs
+++ Program.cs
@@ -36,7 +36,11 @@ namespace Tracking
 
             var changedProperties = firstEntity.GetChangedProperties<TestEntity>(secondEntity);
 
+            var isEqual = firstEntity.UserVariablesEqual(secondEntity);
+
             Console.WriteLine(JsonConvert.SerializeObject(changedProperties));
+            Console.WriteLine("IsEqual: {0}", isEqual);
+            
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+# TrackedEntity
+
+My goal is to have a class TrackedEntity that I can inherit from and use with an attribute (For now it's CompareValues)
+To help track changes to specific entity fields.
+
